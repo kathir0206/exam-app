e6785c77fd441a27f636ed9ae6ee4e9e3d75beea
kathir0206 <kathiresanmsc23@gmail.com>
2023-10-24 17:01:20 +0530

    exam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import hi1 from "./hi.jpeg"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center bg-red-700 ">Hello World!</h1>
+      <img className="h-220 w-223 rounded-300px"  src={hi1} alt="leaf" />
+    </>
   );
-}
+};
 
 export default App;
